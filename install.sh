57428a466129371b572c9da1979ebf211b94e14d
Zorendo <max.mironov28112009@gmail.com>
2026-03-24 19:54:19 +0400

    Переписано меню с пайтона на баш

---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Очистка терминала
+clear_screen() {
+    clear
+}
+
+# Старт сервера
+start_server() {
+    echo "Запуск сервера"
+    bash start.sh
+}
+
+# Установка ядра
+download_core() {
+    (cd bash_scripts && bash core.sh)
+}
+
+# Установка плагинов
+download_plugins() {
+    (cd bash_scripts && bash plugins.sh)
+}
+
+# Обновление репозитория
+download_mirror() {
+    echo "Обновление репозитория..."
+}
+
+while true
+do
+    clear_screen
+
+    echo "
+    МЕНЮ
+1) Запустить сервер
+2) Скачать ядро
+3) Скачать плагины
+4) Обновить Github репозиторий
+5) Скачать все
+6) Выход
+"
+
+    read -p "Выбор: " choice
+
+    case $choice in
+        1)
+            start_server
+            ;;
+        2)
+            download_core
+            ;;
+        3)
+            download_plugins
+            ;;
+        4)
+            download_mirror
+            ;;
+        5)
+            download_core
+            download_plugins
+            download_mirror
+            ;;
+        6)
+            break
+            ;;
+        *)
+            echo "Ошибка"
+            ;;
+    esac
+
+    read -p "Нажмите Enter чтобы продолжить..."
+done
