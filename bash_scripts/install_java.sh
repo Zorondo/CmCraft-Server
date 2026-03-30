434d0f9ad34bb3cd8a2b3b60946d55f8af789f3d
Zorondo <max.mironov28112009@gmail.com>
2026-03-30 12:38:57 +0400

    Добавлена установка зависимостей

---
+++ bash_scripts/install_java.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/bash
+ 
+echo "Устанавливаем зависимости"
+
+if ! command -v git >/dev/null 2>&1; then
+    echo "Java не найдена. Устанавливаю..."
+
+    if command -v apt >/dev/null 2>&1; then
+        sudo apt update
+        sudo apt install openjdk-21-jdk -y 
+
+    elif command -v pacman >/dev/null 2>&1; then
+        sudo pacman -Sy jdk21-openjdk --noconfirm
+
+    else
+        echo "Неизвестный пакетный менеджер"
+        exit 1
+    fi
+
+else
+    echo "Java уже установлена"
+fi
+
+
+if ! command -v git >/dev/null 2>&1; then
+    echo "tmux не найден. Устанавливаю..."
+
+    if command -v apt >/dev/null 2>&1; then
+        sudo apt update
+        sudo apt install tmux -y 
+
+    elif command -v pacman >/dev/null 2>&1; then
+        sudo pacman -Sy tmux --noconfirm
+
+    else
+        echo "Неизвестный пакетный менеджер"
+        exit 1
+    fi
+
+else
+    echo "tmux уже установлен"
+fi
+
+echo "Готово"
